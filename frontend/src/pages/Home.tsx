fff8521a5b6e12e94105654c4c1b1a9f978d818c
Preet Yadav <preet.yadav266@gmail.com>
2024-04-09 23:49:12 +0530

    chagned the logo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Blog } from "../components/ui/Blog"
 import { Button } from "../components/ui/Button"
 import { Input } from "../components/ui/Input"
 import { Link } from "react-router-dom"
-import MediumLogo from "../images/Medium_(website)_logo.svg"
+import MediumLogo from "../images/blogger-icon.webp"
 import { useEffect, useState } from "react";
 import axios from "axios"
 import { BlogSkelton } from "../components/ui/BlogSkelton"
@@ -40,10 +40,11 @@ export function Home() {
         <div className=" space-y-2">
           <div className="flex justify-between">
             <div className="flex  gap-4">
-              <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" /></svg>
-              </div>
-              <img src={MediumLogo} className=" w-36 "></img>
+              
+              <div className="flex">
+                  <img src={MediumLogo} className=" w-10 "></img>
+                  <p className="font-bold">logging</p>
+                </div>
               <Button className="ml-4" size="sm" variant="outline">
                 <Input placeholder="Search" type="text" />
               </Button>
@@ -73,11 +74,12 @@ export function Home() {
         <div className=" space-y-6">
           <div className=" space-y-2">
             <div className="flex justify-between">
-              <div className="flex  gap-4">
-                <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                  <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" /></svg>
+              <div className="flex gap-4">
+                
+                <div className="flex">
+                  <img src={MediumLogo} className=" w-10 "></img>
+                  <p className="font-bold">logging</p>
                 </div>
-                <img src={MediumLogo} className=" w-36 "></img>
                 <Button className="ml-4" size="sm" variant="outline">
                   <Input placeholder="Search" type="text" />
                 </Button>
